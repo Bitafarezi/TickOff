cf75311ce86a75eb0af7ec9fa9e7aa8e2ec81c28
Bita <bitafarezi79@gmail.com>
2026-07-01 14:56:36 +0330

    Add views for rendering page

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from django.views.generic import ListView
+
+# from .models import Category, Book
 
 # Create your views here.
+
+
+def homeView(request):
+    return render(request, 'base.html')
